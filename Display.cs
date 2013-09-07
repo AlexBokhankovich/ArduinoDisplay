6ad225105ba69329f03afa01ea29776fb37ce95e
Alex Bokhankovich <alex.bokhankovich@gmail.com>
2013-09-07 17:49:32 +0400

    Add info class and change some display methods

--- Display.cs
+++ Display.cs
@@ -9,39 +9,62 @@ namespace OrbitalHWMonitor
 {
     class Display
     {
+
+        //display dimensions
+        const byte rows = 4;
+        const byte columns = 20;
+
         //clear screen command sequence
-        byte[] clear_screen = new byte[2] {254, 88};
+        const byte init_byte = 254;
+        const byte cls_byte = 88;
+        const byte set_cursor_byte = 71;
+
+
 
         SerialPort port = new SerialPort();
 
         public Display(string portName, int baudRate)
         {
-            
+
             port.PortName = portName;
             port.BaudRate = baudRate;
             port.DataBits = 8;
             port.Parity = Parity.None;
             port.StopBits = StopBits.One;
+            port.Open();
         }
-
+        /// <summary>
+        /// Writes text to the current cursor position
+        /// </summary>
+        /// <param name="text"></param>
         public void Write(string text)
         {
-            ClearScreen();
-            port.Open();
             System.Text.ASCIIEncoding encoding = new ASCIIEncoding();
             port.Write(encoding.GetBytes(text), 0, encoding.GetByteCount(text));
-            port.Close();
+        }
+        /// <summary>
+        /// Sets the cursor to the desired position
+        /// </summary>
+        /// <param name="column">Column number to move cursor to</param>
+        /// <param name="row">row number to move cursor to</param>
+        public void SetCursorPosition(byte column, byte row)
+        {
+            byte[] cursor_position = new byte[4] { init_byte, set_cursor_byte, column, row };
+            port.Write(cursor_position, 0, cursor_position.Length);
         }
 
         public void ClearScreen()
         {
-            //Open port
-            port.Open();    
+            byte[] cls = new byte[2] { init_byte, cls_byte };
+
             //Send clear screen command to display
-            port.Write(clear_screen, 0, clear_screen.Length);
-            port.Close();
+            port.Write(cls, 0, cls.Length);         
         }
 
+        public void ClosePort()
+        {
+            port.Close();
+        }
 
     }
 }
---
+++ Info.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalHWMonitor
+{
+    class Info
+    {
+        public static string Time(bool showSec = true)
+        {
+            string timePat = string.Empty;
+
+            if (showSec)
+                timePat = @"hh:mm:ss";
+            else
+                timePat = @"hh:mm";
+            return DateTime.Now.ToString(timePat);
+        }
+    }
+}
