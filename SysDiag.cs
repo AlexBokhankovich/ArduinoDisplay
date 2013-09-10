593d7fa9d0349642cb897ec4e37e16eee411b591
Alex Bokhankovich <alex.bokhankovich@gmail.com>
2013-09-11 02:50:03 +0400

    Add sysDiag class for CPU usage monitoring

---
+++ SysDiag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace OrbitalHWMonitor
+{
+    class SysDiag
+    {       
+        private static PerformanceCounter counter;
+               
+        public SysDiag()
+        {
+            counter = new PerformanceCounter();
+            counter.CategoryName = "Processor";
+            counter.CounterName = "% Processor Time";
+            counter.InstanceName = "_Total";
+        }
+        public string getCurrentCpuUsage()
+        {
+            return string.Format(@"CPU:{0:000}%",counter.NextValue());
+        }
+    }
+}
